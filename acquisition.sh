004fafbb20def892762cef617ffe5166753b6501
yoshimasa_i <yoshimasa_i@mbp.local>
2018-09-13 23:45:49 +0900

    :+1: vscode test

--- acquisition.sh
+++ acquisition.sh
@@ -6,7 +6,7 @@ rm -rf /data/url/*
 # dbから詳細ページurlをRESULTへ
 RESULT=`mysql -u root -N -e "SELECT url FROM scraping.list_url;"`
 
-# 対象URLからHTMLを取得する
+# 対象URLからHTMLを取得
 for i in $RESULT
 do
  curl -O http://www.example.net/$i
